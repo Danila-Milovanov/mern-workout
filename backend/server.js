74406dc5a724e8a2acdac15709265ca148b2858a
Danila-Milovanov <9019853@student.zadkine.nl>
2026-03-18 11:29:30 +0100

    fix

--- backend/server.js
+++ backend/server.js
@@ -27,13 +27,3 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('Database verbinding mislukt:', error.message);
   });
 
-  // CORS toestaan voor frontend
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
-
-// Middleware voor JSON
-app.use(express.json());
-
-// Routes
-app.use('/api/workouts', workoutRoutes);
