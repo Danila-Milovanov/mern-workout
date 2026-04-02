b4ae9ad22186176c0782d4fb64445d9ad8f12944
Danila-Milovanov <9019853@student.zadkine.nl>
2026-04-02 11:54:39 +0200

    opdracht-11-begin

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch("http://localhost:4000/api/users/register", {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({ email, password})
+        })
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            setError(data.error);
+        } else {
+            localStorage.setItem("user", JSON.stringify(data));
+            navigate("/");
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Register</h2>
+
+
+        </form>
+    )
+}
