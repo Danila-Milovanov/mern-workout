0627fdd767b07d057d0cf737704a0f981b15ca8e
Danila-Milovanov <9019853@student.zadkine.nl>
2026-04-02 11:08:20 +0200

    opdracht_10

--- backend/src/routes/workoutRoutes.js
+++ backend/src/routes/workoutRoutes.js
@@ -7,12 +7,16 @@ import {
   deleteWorkout
 } from '../controllers/workoutController.js'
 
+import { requireAuth } from '../middleware/requireAuth.js'
+
 const router = express.Router()
 
-router.get('/', getWorkouts)
-router.get('/:id', getWorkout)
-router.post('/', createWorkout)
-router.patch('/:id', updateWorkout)
-router.delete('/:id', deleteWorkout)
+router.use(requireAuth);
+
+router.get('/', getWorkouts);
+router.get('/:id', getWorkout);
+router.post('/', createWorkout);
+router.patch('/:id', updateWorkout);
+router.delete('/:id', deleteWorkout);
 
 export default router;
